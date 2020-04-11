c174c2f3e9b22b183b8135f7e7949e8540aae26d
Orchestrator-CI-Pipeline <lblw-ff@loblaw.ca>
2020-04-10 22:51:52 -0400

    initial commit

---
+++ src/main/kotlin/com/redinterface/spring/demo/configurationsource/ConfigurationsourceApplication.kt
@@ -0,0 +1,11 @@
+package com.redinterface.spring.demo.configurationsource
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigurationsourceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConfigurationsourceApplication>(*args)
+}
---
+++ src/test/kotlin/com/redinterface/spring/demo/configurationsource/ConfigurationsourceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.redinterface.spring.demo.configurationsource
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationsourceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
